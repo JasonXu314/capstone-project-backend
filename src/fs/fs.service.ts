efa52b27bf8aca41cb108ab8fec8525c7d468b4c
Jason Xu <Xinwen31415@gmail.com>
2024-10-02 14:00:56 -0500

    added primitives for manipulating filesystem

---
+++ src/fs/fs.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { existsSync, mkdirSync, readdirSync, statSync } from 'fs';
+
+@Injectable()
+export class FSService {
+	public ensureDir(path: string): void {
+		if (!existsSync(path)) {
+			mkdirSync(path, { recursive: true });
+		}
+	}
+
+	public *traverse(path: string, filter: (path: string) => boolean = () => true): Generator<string, void, never> {
+		if (statSync(path).isDirectory()) {
+			const contents = readdirSync(path);
+
+			for (const elem of contents) {
+				if (filter(`${path}/${elem}`)) {
+					if (statSync(`${path}/${elem}`).isDirectory()) {
+						yield* this.traverse(`${path}/${elem}`, filter);
+					} else {
+						yield `${path}/${elem}`;
+					}
+				}
+			}
+		} else {
+			if (filter(path)) {
+				yield path;
+			}
+		}
+
+		return;
+	}
+}
+
