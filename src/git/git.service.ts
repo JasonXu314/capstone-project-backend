c2983a702e1b2b5f480d4e96784962e98e56cb41
Jason Xu <Xinwen31415@gmail.com>
2024-10-04 21:01:50 -0500

    added cloning functionality to git service

--- src/git/git.service.ts
+++ src/git/git.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { simpleGit, SimpleGit } from 'simple-git';
 import { FSService } from 'src/fs/fs.service';
 
 @Injectable()
 export class GitService {
 	public constructor(private readonly fs: FSService) {
 		fs.ensureDir('repos');
+	}
+
+	private git(path: string): SimpleGit {
+		return simpleGit(`repos/${path}`);
+	}
+
+	public async clone(url: string, to: string): Promise<void> {
+		// NOTE: don't use helper git function because this needs to have base dir as <cwd>/repos
+		const git = simpleGit('repos');
+
+		git.clone(url, to);
 	}
 }
 
