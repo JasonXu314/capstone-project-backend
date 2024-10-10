44ec47b9efb1c094687733376307f5d2463afecb
Jason Xu <Xinwen31415@gmail.com>
2024-10-10 14:00:32 -0500

    added github webhook debugging endpoint & types for push alerts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { AppService } from './app.service';
+import { PushRecord } from './gh/models';
 import { GitService } from './git/git.service';
 
 @Controller()
@@ -10,6 +11,11 @@ export class AppController {
 	@Get('/')
 	public getHello(): string {
 		return this.appService.getHello();
+	}
+
+	@Post('/webhooks')
+	public test(@Body() body: PushRecord): void {
+		console.log(body.commits);
 	}
 
 	@Post('/clone')
