9fc1ffdf89e15dec6e88f467bf87e013d2c84c1a
Jason Xu <Xinwen31415@gmail.com>
2024-10-17 14:02:14 -0500

    added some basic git actions to endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { randomUUID } from 'crypto';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Project } from '@prisma/client';
 import { AppService } from './app.service';
 import { PushRecord } from './gh/models';
 import { GitService } from './git/git.service';
+import { ProjectsService } from './projects/projects.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService, private readonly git: GitService) {}
+	constructor(private readonly appService: AppService, private readonly git: GitService, private readonly projects: ProjectsService) {}
 
 	@Get('/')
 	public getHello(): string {
@@ -16,11 +17,17 @@ export class AppController {
 	@Post('/webhooks')
 	public test(@Body() body: PushRecord): void {
 		console.log(body.commits);
+		this.projects.syncProject(body.repository.url);
 	}
 
 	@Post('/clone')
-	public async clone(@Body('url') url: string): Promise<void> {
-		this.git.clone(url, randomUUID());
+	public async clone(@Body('url') url: string, @Body('name') name: string): Promise<Project> {
+		return this.projects.createProject(name, url);
+	}
+
+	@Delete('/:id')
+	public async delete(@Param('id') id: string): Promise<void> {
+		return this.projects.deleteProject(id);
 	}
 }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AuthMiddleware } from './auth/auth.middleware';
 import { AuthModule, DATA_SOURCE, PREFIX } from './auth/auth.module';
 import { DBModule } from './db/db.module';
 import { GitModule } from './git/git.module';
+import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
 import { serveClient } from './utils/utils';
 
 @Module({
-	imports: [DBModule, GitModule, UsersModule, AuthModule.register({ prefix: 'placeholder' }), ...serveClient()],
+	imports: [DBModule, GitModule, UsersModule, ProjectsModule, AuthModule.register({ prefix: 'placeholder' }), ...serveClient()],
 	controllers: [AppController],
 	providers: [AppService, { provide: DATA_SOURCE, useClass: UsersService }, { provide: PREFIX, useValue: 'placeholder' }]
 })
