947d22ffdb6005be711680c29753d5e0c3b4242c
Jason Xu <Xinwen31415@gmail.com>
2024-10-31 14:14:56 -0500

    added user creation in preparation for github auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { init } from '@paralleldrive/cuid2';
 import { User } from '@prisma/client';
+import { randomBytes } from 'crypto';
 import { AuthDataSource } from 'src/auth/auth.module';
 import { DBService } from 'src/db/db.service';
+import { GHFullUser } from 'src/gh/models';
 
 @Injectable()
 export class UsersService implements AuthDataSource {
-	public constructor(private readonly db: DBService) {}
+	private readonly cuid: () => string;
+
+	public constructor(private readonly db: DBService) {
+		this.cuid = init({ length: 16 });
+	}
+
+	public async getAll(): Promise<User[]> {
+		return this.db.user.findMany();
+	}
 
 	public async auth(token: string): Promise<User | null> {
-		return { id: token, name: token };
+		return this.db.user.findUnique({ where: { token } });
+	}
+
+	public async register(installation_id: number, ghUser: GHFullUser): Promise<User> {
+		return this.db.user.create({ data: { id: this.cuid(), installation_id, github_id: ghUser.id, name: ghUser.login, token: this.generateToken() } });
+	}
+
+	public async login(ghUser: GHFullUser): Promise<User | null> {
+		return this.db.user.findUnique({ where: { github_id: ghUser.id } });
+	}
+
+	public generateToken(): string {
+		return randomBytes(32).toString('hex');
 	}
 }
 
